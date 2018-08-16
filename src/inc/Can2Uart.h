af661408f34ab5342ebb858eec884bee2d21f9c0
JackZhao <kobe2014zyp@126.com>
2018-08-16 19:34:33 +0800

    v1.0 wmr can2uart

--- src/inc/Can2Uart.h
+++ src/inc/Can2Uart.h
@@ -3,7 +3,7 @@
 
 #include "MPC5604C_M27V.h"
 
-#define CANMSG_MAX 20
+#define CANMSG_MAX 50
 
 void msgBufferInit(void);
 void clearBuffer(void);
@@ -11,4 +11,5 @@ uint32_t setReceiveSize(uint32_t size);
 uint8_t PutCanMsgToBuffer(CAN_RXMSG *msg);
 uint8_t GetUartMsgFromBuffer(UartMwrMsg *mwrmsg);
 uint32_t getMsgSum(void);
+
 #endif
